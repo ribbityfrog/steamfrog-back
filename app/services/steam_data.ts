c62bbd477df1faab740502b1561ba19e709f4d06
LeX <pierre@lastexile.fr>
2025-01-13 17:57:44 +0100

    Endpoints done

--- app/services/steam_data.ts
+++ app/services/steam_data.ts
@@ -6,25 +6,84 @@ class SteamData {
   private _apiKey = env.get('STEAM_KEY')
 
   async getList(last_appid: number = 0, max_results: number = 10000): Promise<SteamList | null> {
-    const endpoint = this._buildEndpoint('list', { last_appid, max_results })
-    const parsedResult = await this._fetchEndpoint(endpoint)
+    const result = await this._buildAndFetch('list', { last_appid, max_results })
 
-    if (parsedResult === null) return null
-
-    return parsedResult.response
+    return result?.response ?? null
   }
 
-  private async _fetchEndpoint(endpoint: string): Promise<any | null> {
+  async getApp(appids: number): Promise<any | null> {
+    const result = await this._buildAndFetch('app', { appids })
+
+    return result?.[String(appids)]?.data ?? null
+  }
+
+  async getReviews(gameid: number) {
+    const result = await this._buildAndFetch('reviews', {}, String(gameid))
+
+    return result?.query_summary ?? null
+  }
+
+  async getAchievements(gameid: number): Promise<any | null> {
+    const [achievementsResult, schemaParsedResult] = await Promise.all([
+      this._buildAndFetch('achievements', { gameid }, '', false),
+      this.getSchema(gameid),
+    ])
+
+    if (achievementsResult.status === 403) return []
+    else if (achievementsResult.status !== 200) return null
+
+    const achievementsParsedResult = await achievementsResult.json()
+
+    const achievements = achievementsParsedResult?.achievementpercentages?.achievements
+
+    if (achievements === undefined || schemaParsedResult === null) return null
+
+    return achievements
+      .filter((achievement: any) => achievement !== null)
+      .map((achievement: any) => {
+        const schema = schemaParsedResult.find((s: any) => s.name === achievement.name)
+
+        if (schema === undefined) return null
+
+        return {
+          name: schema.displayName,
+          description: schema.description,
+          hidden: Boolean(schema.hidden),
+          percent: Math.round(achievement.percent * 100) / 100,
+        }
+      })
+  }
+
+  async getSchema(appid: number): Promise<any | null> {
+    const result = await this._buildAndFetch('schema', { appid })
+
+    return result?.game?.availableGameStats?.achievements ?? null
+  }
+
+  async _buildAndFetch(
+    endpointKey: SteamEndpointKeys,
+    optionalParams: Record<string, string | boolean | number> = {},
+    extraPath: string = '',
+    parse: boolean = true
+  ) {
+    const endpoint = this._buildEndpoint(endpointKey, optionalParams, extraPath)
+    return await this._fetchEndpoint(endpoint, parse)
+  }
+
+  private async _fetchEndpoint(endpoint: string, parse: boolean = true): Promise<any | null> {
     const response = await fetch(endpoint)
 
-    if (response.status !== 200) return null
+    if (parse) {
+      if (response.status !== 200) return null
 
-    return await response.json()
+      return await response.json()
+    } else return response
   }
 
   private _buildEndpoint(
     endpointKey: SteamEndpointKeys,
-    optionalParams: Record<string, string | boolean | number> = {}
+    optionalParams: Record<string, string | boolean | number> = {},
+    extraPath: string = ''
   ): string {
     const ep = steamEndpoints[endpointKey]
 
@@ -37,6 +96,7 @@ class SteamData {
 
     return (
       ep.route +
+      extraPath +
       (params.length > 0
         ? '?' + params.map(([key, value]) => `${key}=${String(value)}`).join('&')
         : '')
--- app/services/steam_data/endpoints.ts
+++ app/services/steam_data/endpoints.ts
@@ -11,6 +11,28 @@ const steamEndpoints = {
     },
     key: true,
   },
+  app: {
+    route: 'https://store.steampowered.com/api/appdetails/',
+    params: { cc: 'us' },
+    key: false,
+  },
+  reviews: {
+    route: 'https://store.steampowered.com/appreviews/',
+    params: { json: 1, language: 'all' },
+    key: false,
+  },
+  achievements: {
+    route: 'https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/',
+    params: {
+      json: 1,
+    },
+    key: false,
+  },
+  schema: {
+    route: 'https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2',
+    params: {},
+    key: true,
+  },
 } satisfies Record<string, SteamEndpoint>
 
 export { steamEndpoints as default }
