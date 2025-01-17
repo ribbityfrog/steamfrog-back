67d65a0ea16f26ace85f742c413d0a49472c0a61
LeX <pierre@lastexile.fr>
2025-01-17 09:18:59 +0100

    adding more bree message debug

--- app/services/bree/index.ts
+++ app/services/bree/index.ts
@@ -64,7 +64,7 @@ export default class Bree {
       })
       .catch((error) =>
         Except.serviceUnavailable('none', {
-          debug: { message: '[service] Bree - Failed to start', error },
+          debug: { message: `[service] Bree - Failed to start : ${error.message}`, error },
         })
       )
   }
